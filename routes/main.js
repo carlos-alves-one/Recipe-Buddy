8bf20240e0ad5b901effc9980b4c2ffa9e3953e5
Carlos Alves <carlos.alves.one@gmail.com>
2022-12-18 13:36:07 +0000

    Declare variables bcrypt and request

--- routes/main.js
+++ routes/main.js
@@ -4,3 +4,9 @@
 // Student...: cdeol003
 // Created...: 18/12/2022
 
+// declare variable for bcrypt
+const bcrypt = require('bcrypt');
+
+// declare variable for request
+const request = require('request');
+
