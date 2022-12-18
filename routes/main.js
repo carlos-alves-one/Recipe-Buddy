7b79bf88a549a0892a6f37dccef443d03758e005
Carlos Alves <carlos.alves.one@gmail.com>
2022-12-18 20:34:50 +0000

    Declare handle our routes

--- routes/main.js
+++ routes/main.js
@@ -10,3 +10,33 @@ const bcrypt = require('bcrypt');
 // declare variable for request
 const request = require('request');
 
+// --->>> Handle our routes .....................................................................................
+
+// define the routes
+module.exports = function (app, shopData) {
+    // declare variable to validate email
+    const { check, validationResult, Result } = require('express-validator');
+  
+    // declare variable to redirect login
+    const redirectLogin = (req, res, next) => {
+      // check the user didn't started a new session
+      if (!req.session.userId) {
+        // if not true redirect to login
+        res.redirect('./login');
+      }
+      // user already started a new session
+      else {
+        next();
+      }
+    };
+  
+    // use the Express Router to handle our routes
+    app.get('/', function (req, res) {
+      // render the index page
+      res.render('index.ejs', shopData);
+    });
+  
+    // use the Express Router to handle our routes
+    app.get('/about', function (req, res) {
+      res.render('about.ejs', shopData);
+    });
