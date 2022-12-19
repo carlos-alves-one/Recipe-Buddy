c250c7f7c00567fe72168a1baf00031a752bf43e
Carlos Alves <carlos.alves.one@gmail.com>
2022-12-19 11:27:53 +0000

    Create register.ejs file

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ const bcrypt = require('bcrypt');
 // declare variable for request
 const request = require('request');
 
-// --->>> Handle our routes .....................................................................................
+// --->>> Handle our routes .............................................................................................................................
 
 // define the routes
 module.exports = function (app, shopData) {
@@ -40,4 +40,193 @@ module.exports = function (app, shopData) {
     app.get('/about', function (req, res) {
       res.render('about.ejs', shopData);
     });
+
+    // --->>> REGISTER NEW USER ........................................................................................................................
+
+    // use the Express Router to handle our routes
+    app.get('/register', function (req, res) {
+      // declare array initialvalues to store data
+      let initialvalues = {
+        username: '',
+        firstname: '',
+        lastname: '',
+        email: '',
+        username: '',
+        password: '',
+      };
+
+      // pass the data to the EJS page and view it
+      return renderRegisteruser(res, initialvalues, '');
+    });
+
+    // --->>> Helper function .........................................................................................................................
+
+    // function to render the register user page
+    function renderRegisteruser(res, initialvalues, errormessage) {
+      // store the data in the shopData object
+      let data = Object.assign({}, shopData, initialvalues, {
+        errormessage: errormessage,
+      });
+
+      // render the register page
+      res.render('register.ejs', data);
+
+      // return the data
+      return;
+    }
+
+    // use the Express Router to handle our routes
+    app.post(
+      '/registered',
+
+      // check is a valid email
+      [
+        check('email')
+          // use sanitize ensures the email address is in a safe and standard format
+          .normalizeEmail(),
+      ],
+
+      // check if the password is not empty
+      [check('password').not().notEmpty().withMessage('Password is required')],
+
+      // check if the username is not empty and at least 5 characters
+      [
+        check('username')
+          .notEmpty()
+          // use sanitize to trim the username
+          .trim()
+          // use sanitize to escape the username
+          .escape()
+          .withMessage('Username is required')
+          .isLength({ min: 5 })
+          .withMessage('Username must be at least 5 characters long'),
+      ],
+
+      // check the password must be 8+ chars long and contain a number
+      [
+        check(
+          'password',
+          'The password must be 8+ chars long and contain a number'
+        )
+          // use sanitize to trim the password
+          .trim()
+          // use sanitize to escape the password
+          .escape()
+          .not()
+          .isIn(['123', 'password', 'abc123'])
+          .withMessage('Do not use a common word as the password. ')
+          .isLength({ min: 8 })
+          .matches(/\d/)
+          .withMessage('Must contain a number. '),
+      ],
+
+      // call function request and response
+      function (req, res) {
+        // store the errors in a dictionary
+        const errors = validationResult(req);
+
+        // check we have errors in the dictionary
+        if (!errors.isEmpty()) {
+          // redirect to register page
+          res.redirect('./register');
+
+          // print errors dictionary for debug purposes
+          console.log(errors);
+
+          // check we have an invalid email
+          if (errors.errors[0].param == 'email') {
+            console.log(
+              '>>> ERROR: Email is invalid. Please enter again the data'
+            );
+          }
+
+          // check we have an invalid password length
+          if (
+            errors.errors[0].param == 'password' ||
+            errors.errors[1].param == 'password'
+          ) {
+            console.log(
+              '>>> ERROR: The password must be 8+ chars long and contain a number. Please enter again'
+            );
+          }
+
+          // check username is different from password
+          if (req.body.username == req.body.password) {
+            console.log(
+              '>>> ERROR: The password and username cannot be the same. Please enter again'
+            );
+          }
+
+          // we have a valid input
+        } else {
+          // declare variables to use with the function hash of bcrypt
+          const saltRounds = 10;
+          const plainPassword = req.body.password;
+
+          // hash the password
+          bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+            // sanitize the input
+            let username = req.sanitize(req.body.username);
+            let firstname = req.sanitize(req.body.firstname);
+            let lastname = req.sanitize(req.body.lastname);
+            let email = req.sanitize(req.body.email);
+
+            // declare array initialvalues to store data
+            var params = [
+              req.body.username,
+              req.body.firstname,
+              req.body.lastname,
+              req.body.email,
+              hashedPassword,
+            ];
+
+            // query database to create a new record
+            sqlquery =
+              'INSERT INTO users (username, firstname, lastname, email, hashedPassword) VALUES (?,?,?,?,?)';
+
+            // execute sql query
+            db.query(sqlquery, params, (err, result) => {
+              // if error
+              if (err) {
+                // return error
+                return console.error(err.message);
+
+                // if not error
+              } else {
+                // print welcome message on the console
+                console.log(
+                  '# Hello ' +
+                    req.body.firstname +
+                    ' ' +
+                    req.body.lastname +
+                    ' you are now registered!'
+                );
+
+                // more print messages
+                console.log(
+                  '# You will receive an email at..: ' + req.body.email
+                );
+                console.log(
+                  '# Your username is..............: ' + req.body.username
+                );
+                console.log(
+                  '# Your password is..............: ' + req.body.password
+                );
+                console.log(
+                  '# Hashed password is............: ' + hashedPassword
+                );
+
+                // store the username in a variable to be used with the EJS pages
+                loggedinuser = req.body.username;
+
+                // render the new user page
+                res.render('newUser.ejs', shopData);
+              }
+            });
+          });
+        }
+      }
+    );
+
+// end of module.exports
 };
