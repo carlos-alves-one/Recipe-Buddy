f0a3dd148b8fb517fb628ced6d3f0dff46a2db23
Carlos Alves <carlos.alves.one@gmail.com>
2022-12-26 14:32:13 +0000

    Update route search food without direct match selected

--- routes/main.js
+++ routes/main.js
@@ -595,8 +595,8 @@ module.exports = function (app, shopData) {
                   // print message
                   console.log('>>> Ingredient searched successfully');
   
-                  // render update food page
-                  res.render('updateFood.ejs', newData);
+                  // render the search food result page
+                  res.render('searchFood-Result.ejs', newData);
                 }
               }
           });
